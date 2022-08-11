b1a796891d26c6cfe895f2a19a63de692c85c8dd
dominicroemer <dominicroemer@gmail.com>
2022-08-10 18:03:26 -0700

    Add darkmode toggle, set mediaQuery for mobile devices

--- src/App.js
+++ src/App.js
@@ -1,14 +1,45 @@
 import logo from './logo.svg';
-
+import React, {useState} from 'react';
 import {link} from "react-router-dom";
 import './App.css';
 
 
 function App() {
+
+  const [darkMode, toggleDarkMode] = useState(false);
+  const [darkStyle, setDarkStyle] = useState({
+    backgroundColor: '',
+    color: '',
+  })
+
+  function handleDarkModeToggle(e){
+    e.preventDefault();
+    if(darkMode) {
+      setDarkStyle({
+        backgroundColor: '',
+        color: '',
+      })
+      toggleDarkMode(!darkMode);
+    } else {
+      setDarkStyle({
+        backgroundColor: '#333',
+        color: '#fff',
+      })
+      toggleDarkMode(!darkMode);
+    }
+  }
+
+
+
+
   return (
-    <div className="App">
+    <div className="App"
+      // style={darkStyle}
+      style={darkMode ? {background: '#333', color: '#fff'} : {}}
+      >
 
       <nav className='appNav'
+      style={darkMode ? {background: '#1c1c1c'} : {}}
       //   style={{
       //   borderBottom: "solid 1px",
       //   paddingBottom: "1rem",
@@ -28,15 +59,22 @@ function App() {
           </h2>
         </div>
         <div className='navOtherThings navContainer'>
-          <h2>
-            Other things?
-          </h2>
           <h5>
           This webpage is designed to help with the King's Fall Day 1 Raid race
           </h5>
+          <button onClick={e => handleDarkModeToggle(e)}>
+            Darkmode Toggle
+          </button>
+
         </div>
 
       </nav>
+      <div className='appContent'>
+        <p>
+          something here
+        </p>
+        <p> and here</p>
+      </div>
 
       {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
