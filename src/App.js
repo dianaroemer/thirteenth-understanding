6eba7e384b936d86b2f3217ea6e69b316d2e0099
dominicroemer <dominicroemer@gmail.com>
2022-08-10 17:23:16 -0700

    Begin fleshing out App Nav

--- src/App.js
+++ src/App.js
@@ -1,10 +1,44 @@
 import logo from './logo.svg';
+
+import {link} from "react-router-dom";
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+
+      <nav className='appNav'
+      //   style={{
+      //   borderBottom: "solid 1px",
+      //   paddingBottom: "1rem",
+      // }}
+      >
+        <div className='titleContainer navContainer'>
+          <h1 className='navTitle'>
+            13th Understanding
+          </h1>
+          <div className='titleQuote'>
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;—&nbsp;"To rend one's enemies is to see them not as equals, but objects—hollow of spirit and meaning."
+          </div>
+        </div>
+        <div className='navClockContainer navContainer'>
+          <h2>
+            CLOCK
+          </h2>
+        </div>
+        <div className='navOtherThings navContainer'>
+          <h2>
+            Other things?
+          </h2>
+          <h5>
+          This webpage is designed to help with the King's Fall Day 1 Raid race
+          </h5>
+        </div>
+
+      </nav>
+
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +51,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
