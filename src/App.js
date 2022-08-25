72b48c0f855c53ea2680aaecce6077eaa721b874
D Roemer <dominicroemer@gmail.com>
2022-08-25 02:16:51 -0700

    Another react-ga analytics test

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import Menu from './Components/Menu';
 import BreakMenu from './Components/BreakMenu';
 
 import ReactGA from 'react-ga';
-  const TRACKING_ID = "UA-238648834-1"; // OUR_TRACKING_ID
+  const TRACKING_ID = "UA-238648834-2"; // OUR_TRACKING_ID
   ReactGA.initialize(TRACKING_ID);
 
 
